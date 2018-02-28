3610f43a8d13bf0cc32a8ea6b7e8a4cfe17e94a0
eduardo-anton <ebanton@gmail.com>
2018-02-28 18:10:16 -0300

    Add files via upload

---
+++ target/classes/import.sql
@@ -0,0 +1,2 @@
+insert into item (etiqueta, descricao, data_Aquisicao) values ('NT0123', 'Notebook i7', sysdate());
+insert into item (etiqueta, descricao, data_Aquisicao) values ('AU9875', 'Ford Fiesta', sysdate());
